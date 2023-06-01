5fe0d7bbf09e76f02eeedf5a2f9039bda82b4de6
voskanvl <voskanvl@gmail.com>
2023-06-01 17:25:40 +0300

    connect slider1

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,10 @@
-import "./sass/style.sass";
+import "./sass/style.sass"
+import "@splidejs/splide/css"
+import Splide, { Options } from "@splidejs/splide"
 
+const slider1 = new Splide("#slider1", {
+    type: "loop",
+    pagination: true,
+    arrows: true,
+})
+slider1.mount()
