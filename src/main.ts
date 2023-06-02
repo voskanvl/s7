110b0bdbe9522d2e073309e6ac1e15caeed92068
voskanvl <voskanvl@gmail.com>
2023-06-02 14:58:03 +0300

    slider2 2

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,10 @@ const slider1 = new Splide("#slider1", {
     arrows: true,
 })
 slider1.mount()
+
+const slider2 = new Splide("#slider2", {
+    type: "loop",
+    pagination: true,
+    arrows: true,
+})
+slider2.mount()
