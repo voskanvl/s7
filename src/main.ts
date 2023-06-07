06f1d32eb1007bd0d65d9b10f7062a6f5afda91d
voskanvl <voskanvl@gmail.com>
2023-06-07 15:42:22 +0300

    fonts + favicon

--- src/main.ts
+++ src/main.ts
@@ -17,3 +17,20 @@ const slider2 = new Splide("#slider2", options)
 slider2.mount()
 
 intersection()
+
+//--- change arrows ---
+
+const leftButtons = document.querySelectorAll<HTMLElement>(".splide__arrow.splide__arrow--prev")
+const rightButtons = document.querySelectorAll<HTMLElement>(".splide__arrow.splide__arrow--next")
+leftButtons &&
+    leftButtons.forEach(e => {
+        e.innerHTML = `<img src="../assets/icons/arrow-left.png" alt="arrow - left">`
+    })
+rightButtons &&
+    rightButtons.forEach(e => {
+        e.innerHTML = `<img src="../assets/icons/arrow-right.png" alt="arrow - right">`
+    })
+
+// <svg width="12" height="22" viewBox="0 0 12 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+// <path d="M1 21L11 11L1 1" stroke="#54565A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+// </svg>
